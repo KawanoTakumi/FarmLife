110fff7ee0e580adfe4a384f94f446fcffb4348e
DESKTOP-8UJEPPT\GA3 <h1202416001@hiratagakuen.onmicrosoft.com>
2026-04-13 12:14:58 +0900

    マップの原型を作成中　プレイヤーの移動とインタラクトを作成中

---
+++ FarmLife/Source/FarmLife/PlayerMove.h
@@ -0,0 +1,32 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Pawn.h"
+#include "PlayerMove.generated.h"
+
+UCLASS()
+class FARMLIFE_API APlayerMove : public APawn
+{
+	GENERATED_BODY()
+
+public:
+	// Sets default values for this pawn's properties
+	APlayerMove();
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+public:	
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+	// Called to bind functionality to input
+	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
+
+private:
+	//プレイヤーステータス
+	float move_speed = 1.0f;//移動速度
+};
