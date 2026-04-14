1fee3553bbe01d70c6d81b161a4b92ccefbc59f0
KawanoTakumi <h1202416001@hiratagakuen.onmicrsoft.com>
2026-04-14 10:21:52 +0900

    作物生成用のボックス生成中

---
+++ FarmLife/Source/FarmLife/SpawnField.cpp
@@ -0,0 +1,59 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "SpawnField.h"
+#include "BaseCrop.h"
+// Sets default values
+ASpawnField::ASpawnField()
+{
+ 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+	FieldMesh = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("FieldMesh"));
+	RootComponent = FieldMesh;
+
+	SpawnArea = CreateDefaultSubobject<UBoxComponent>(TEXT("SpawnArea"));
+	SpawnArea->SetupAttachment(FieldMesh);
+}
+
+// Called when the game starts or when spawned
+void ASpawnField::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void ASpawnField::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
+FVector ASpawnField::GetRandomPointInField() const
+{
+	FVector origin = SpawnArea->GetComponentLocation();
+	FVector extent = SpawnArea->GetScaledBoxExtent();
+
+	float X = FMath::RandRange(-extent.X, extent.X);
+	float Y = FMath::RandRange(-extent.Y, extent.Y);
+
+	return origin + FVector(X, Y, 0.0f);
+}
+
+void ASpawnField::SpawnCrops(int32 count)
+{
+	for (int i = 0; i < count; i++)
+	{
+		FVector Location = GetRandomPointInField();
+
+		int32 index = FMath::RandRange(0, CropClass.Num() - 1);
+		TSubclassOf<ABaseCrop> select = CropClass[index];
+
+		GetWorld()->SpawnActor<ABaseCrop>(
+		select,
+		Location,
+		FRotator::ZeroRotator
+		);
+	}
+}
---
+++ FarmLife/Source/FarmLife/SpawnField.h
@@ -0,0 +1,41 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "Components/BoxComponent.h"
+#include "Components/StaticMeshComponent.h"
+#include "BaseCrop.h"
+#include "SpawnField.generated.h"
+
+UCLASS()
+class FARMLIFE_API ASpawnField : public AActor
+{
+	GENERATED_BODY()
+	
+public:	
+	// Sets default values for this actor's properties
+	ASpawnField();
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+public:	
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+	FVector GetRandomPointInField()const;
+
+	void SpawnCrops(int32 count);
+
+	UPROPERTY(VisibleAnywhere)
+	UStaticMeshComponent* FieldMesh;
+
+	UPROPERTY(VisibleAnywhere)
+	UBoxComponent* SpawnArea;
+
+	UPROPERTY(EditAnywhere)
+	TArray<TSubclassOf<ABaseCrop>> CropClass;
+};
